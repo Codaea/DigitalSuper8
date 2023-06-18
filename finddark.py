575af64fe63a80539dcb40c1f312d3daef833cc1
Codaea <93889672+Codaea@users.noreply.github.com>
2023-06-18 13:45:58 -0700

    fixed finddark

--- finddark.py
+++ finddark.py
@@ -2,7 +2,6 @@ import picamera
 import time
 from PIL import Image
 
-
 def calculate_average_pixel_value(image):
     # Convert the image to grayscale for simplicity
     grayscale_image = image.convert('L')
@@ -20,22 +19,23 @@ def is_dark(pixel_value):
     # You can adjust the threshold values as per your requirement
     darkness_threshold = 50  # Adjust this value as needed
     return pixel_value < darkness_threshold
+
 def find_val():
     with picamera.PiCamera() as camera:
-            camera.resolution = (1920, 1080)
-            camera.framerate = 5
-            camera.start_preview()
-            time.sleep(2)  # Wait for the camera to adjust to the light conditions
-            
-    while True:
+        camera.resolution = (1920, 1080)
+        camera.framerate = 5
+        camera.start_preview()
+        time.sleep(2)  # Wait for the camera to adjust to the light conditions
+        
+        while True:
             camera.capture('image.jpg', use_video_port=True)  # Capture a frame
             # Open the captured image using PIL
             image = Image.open('image.jpg')
-            #Calculate the average pixel value
+            # Calculate the average pixel value
             pixel_value = calculate_average_pixel_value(image)
             # Print Value to console
-            result = pixel_value +" SO.. "+is_dark(pixel_value)
-            return result
+            result = f"{pixel_value} SO.. {is_dark(pixel_value)}"
+            print(result)
             time.sleep(1)
 
 find_val()
